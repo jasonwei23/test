bc4d90a8f31605a292d65bfc5d53a87aa69f7e48
jasonwei23 <145849576+jasonwei23@users.noreply.github.com>
2024-03-11 23:18:00 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,2 +1,8 @@
 # QUIC
 echo -e '#QUIC\nnet.core.rmem_max=4194304\nnet.core.wmem_max=4194304' > openwrt/package/base-files/files/etc/sysctl.d/20-net-core-mem-max.conf
+
+# Default network segment
+sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+
+# Delete ula prefix
+sed -i 's/network.globals.ula_prefix=\'auto\'/network.globals.ula_prefix=\'\'/g' package/base-files/files/bin/config_generate
