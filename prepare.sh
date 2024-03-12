e6425bca6f311b0ac2c4f76d6ad0abaf30d599c9
jasonwei23 <145849576+jasonwei23@users.noreply.github.com>
2024-03-12 23:38:58 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -6,4 +6,6 @@ sed -i 's/192.168.1.1/192.168.99.1/g' openwrt/package/base-files/files/bin/confi
 
 # Modify default settings
 sed -i '$i uci set luci.sauth.sessiontime=\x270\x27' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
-sed -i '$i uci commit luci\n' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
+sed -i '$i uci commit luci' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
+sed -i '$i uci delete network.globals.ula_prefix' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
+sed -i '$i uci commit network' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
