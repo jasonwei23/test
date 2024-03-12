62fbea94ebf69b8d70f68c4f95227f1441773607
jasonwei23 <145849576+jasonwei23@users.noreply.github.com>
2024-03-12 23:52:57 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -9,3 +9,5 @@ sed -i '$i uci set luci.sauth.sessiontime=\x270\x27' openwrt/package/emortal/def
 sed -i '$i uci commit luci' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
 sed -i '$i uci delete network.globals.ula_prefix' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
 sed -i '$i uci commit network' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
+sed -i '$i uci set firewall.@defaults[0].flow_offloading_hw=\x270\x27' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
+sed -i '$i uci commit firewall' openwrt/package/emortal/default-settings/files/99-default-settings-chinese
